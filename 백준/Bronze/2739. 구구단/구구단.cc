dc7074c39c51ba7b9e8fb187b2cb480b7a775a6b
jaejae12 <95065252+jaejae12@users.noreply.github.com>
2024-07-08 22:36:22 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, S;
+    cin >> N;
+    for (int i = 1; i < 10; i++) {
+        S = N * i;
+        cout  << N << " * " << i << " = " << S << endl;
+    }
+
+
+    }
