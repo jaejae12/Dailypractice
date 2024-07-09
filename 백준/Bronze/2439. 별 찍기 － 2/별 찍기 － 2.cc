91fb2d2948d7168b162b212e24667ab19f66001b
jaejae12 <95065252+jaejae12@users.noreply.github.com>
2024-07-09 23:56:40 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = N; j > i + 1; j--) {
+            cout << " ";
+        }
+        for (int k = 0; k < i + 1; k++) {
+            cout << "*";
+        }
+        if(i < N-1) cout << endl;
+    }
+
+}
