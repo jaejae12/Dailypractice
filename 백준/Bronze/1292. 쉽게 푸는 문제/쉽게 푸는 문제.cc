cca72d6b8c295300290fa8ca3d453424a7c05665
jaejae12 <95065252+jaejae12@users.noreply.github.com>
2024-07-19 20:21:16 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.cc
@@ -10,22 +10,18 @@ int main()
 {
 	int A, B;
 	cin >> A >> B;
-	int i = 1;
-	int j = 1;
-	int DP[1001];
-	DP[0] = 0;
-	DP[1] = 1;
-	while (i <= 1000) {
-		for (int k = 0; k < j; k++) {
-			DP[i] = DP[i-1] + j;
-			i++;
-			if (i > 1000) break;
+	int tot = 0;
+	int n = 0;
+	for (int i = 1; i <=B ; i++) {
+		for (int j = 1; j <= i; j++) {
+			n++;
+			if ((n >= A) && (n <= B)) {
+				tot = tot + i;
+			}
+			
 		}
-			j++;
-		
 	}
-	cout << DP[B] - DP[A - 1];
-	
 
+	cout << tot;
 	return 0;
 }
